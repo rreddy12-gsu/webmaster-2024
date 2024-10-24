a008381f53a6e9aa9596600688fcbba74b399b98
rreddy12-gsu <rreddy12@student.gsu.edu>
2024-10-24 21:52:09 +0000

    express app

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,13 @@
-const express=require('express'), 
-http=require('http'); 
-const hostname='localhost'; 
-const port=8080; 
-const app=express(); 
+const express = require("express");
+const port = 8080;
+const app = express();
 
-app.use((req, res)=> { 
-		console.log(req.headers); 
-		res.statusCode=200; 
-		res.setHeader('Content-Type', 'text/html'); 
-		res.end('<html><body><h1>This is a test server</h1></body></html>'); 
-}); 
-const sample_server=http.createServer(app); 
-
-sample_server.listen(port, hostname, ()=> { 
-		console.log(`Server running at http: //${hostname}:${port}/`);});
+app.use((req, res) => {
+  res.statusCode = 200;
+  res.json({
+    name: "test",
+  });
+});
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
